d34b6a331b022d8df9ef11191a340b08729497b3
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2022-05-28 08:57:56 +0900

    init

---
+++ src/popup/Popup.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="w-[500px] h-[600px] text-center text-gray-700">
+    <iframe src="https://wordmonster.herokuapp.com" frameborder="0" />
+  </main>
+</template>
